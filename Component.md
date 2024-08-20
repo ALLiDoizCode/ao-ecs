39a0080897a5cbb387380df10013b868ad73a95e
Jonathan Green <dev.jonathan.green@gmail.com>
2024-08-20 11:30:34 -0400

    added handler to remove components

---
+++ Component.md
@@ -0,0 +1,19 @@
+```lua
+local Component = {
+    Handlers = { "Transfer", "Mint", "Burn", "Credit-Notice", "Debit-Notice", "Mint-Notice", "Burn-Notice" },
+    System = "Mq-EhTkmZ5nSBmnqzp3vl_d35WwMTGphtk90Z7wVr-o",
+    Title = "Token Component",
+    Description = "This component provides your process the ability to act as a fungible token",
+    Data = {
+        Id = "",
+        Balances = {},
+        Minter = "",
+        Denomination = "",,
+        TotalSupply = "0",
+        Name = "",,
+        Ticker = "",,
+        Logo = "",,
+        Description = "",
+    }
+};
+```
--- entity.lua
+++ entity.lua
@@ -7,11 +7,21 @@ if not ComponentId then ComponentId = 1 end;
 if not Components then Components = {} end;
 
 Handlers.add("AddComponent", Handlers.utils.hasMatchingTag('Action', "AddComponent"), function(msg)
+    assert(msg.From == Owner,"Not Authorized");
+    local component = json.decode(msg.Data);
     local currentId = tostring(ComponentId);
     ComponentId = ComponentId + 1;
-    local tokenComponent = TokenComponent(currentId, msg.Entity, msg.Minter, msg.Denomination, msg.Name, msg.Ticker,
-        msg.Logo, msg.Description);
-    AddComponent(tokenComponent);
+    component["Id"] = currentId;
+    AddComponent(component);
+end)
+
+Handlers.add("RemoveComponent", Handlers.utils.hasMatchingTag('Action', "RemoveComponent"), function(msg)
+    assert(msg.From == Owner,"Not Authorized");
+    local component = Components[msg.Id]
+    for i, v in ipairs(component.Handlers) do
+        Handlers.remove(v)
+    end
+    Components[msg.Id] = {}
 end)
 
 Handlers.add("Components", Handlers.utils.hasMatchingTag('Action', "Components"), function(msg)
@@ -20,35 +30,11 @@ Handlers.add("Components", Handlers.utils.hasMatchingTag('Action', "Components")
         Data = json.encode(Components),
     });
 end)
-
-function TokenComponent(Id, Entity, Minter, Denomination, Name, Ticker, Logo, Description)
-    local Component = {
-        Handlers = { "Transfer", "Mint", "Burn", "Credit-Notice", "Debit-Notice", "Mint-Notice", "Burn-Notice" },
-        System = "Mq-EhTkmZ5nSBmnqzp3vl_d35WwMTGphtk90Z7wVr-o",
-        Title = "Token Component",
-        Description = "This component provides your process the ability to act as a fungible token",
-        Data = {
-            Entity = Entity,
-            Id = Id,
-            Balances = {},
-            Minter = Minter,
-            Denomination = Denomination,
-            TotalSupply = "0",
-            Name = Name,
-            Ticker = Ticker,
-            Logo = Logo,
-            Description = Description
-        }
-    };
-
-    return Component
-end
-
 function AddComponent(component)
     Components[component.Data.Id] = component;
     for i, v in ipairs(component.Handlers) do
         Handlers.add(v, Handlers.utils.hasMatchingTag('Action', v), function(msg)
-            if(msg.From == component.System) then
+            if (msg.From == component.System) then
                 local data = json.decode(msg.Data);
                 Components[data.Id] = data;
             else
@@ -63,3 +49,5 @@ function AddComponent(component)
         end)
     end
 end
+
+
